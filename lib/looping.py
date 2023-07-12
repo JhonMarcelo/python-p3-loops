530955b9cacb77200e28fff94611d186c0120eac
Jhon Waree Marcelo <jhon.waree.marcelo@gmail.com>
2023-07-12 15:39:46 -0400

    Complete Control Flow:Loops

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
-
+    counter = 10
+    while counter > 0:
+         print(f"{counter}")
+         counter -= 1
+    print("Happy New Year!")
+   
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_int = [pow(integer,2) for integer in int_list]
+    return squared_int
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(100):
+        i = i+1
+        if (i % 3 == 0) and (i % 5 == 0):
+            print("FizzBuzz")
+        elif (i % 3 == 0):
+            print("Fizz")
+        elif (i % 5 == 0):
+            print("Buzz")
+        else:
+            print(f"{i}")
+    
+
+
